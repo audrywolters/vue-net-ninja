5fb12e41354abf72e5bbb1a12ca4bb692096eab6
Audry <audrywolters@gmail.com>
2022-05-23 09:57:55 -0500

    v-for li

--- app.js
+++ app.js
@@ -2,28 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            bookList: [
+                { title: 'name of the wind', author: 'patrick rothfuss' },
+                { title: 'the way of kinds', author: 'brandon sanderson' },
+                { title: 'the final empire', author: 'brandon sanderson' }
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e.type)
 
-            if (data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
     }
 })
 
