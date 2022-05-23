702d938d0fa8d7bcb37ae1d45876a8d10d9321f7
Audry <audrywolters@gmail.com>
2022-05-23 16:23:51 -0500

    binding class and toggle fav

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             bookList: [
-                { title: 'name of the wind', author: 'patrick rothfuss',  img: 'assets/1.jpg' },
-                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
-                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' }
+                { title: 'name of the wind', author: 'patrick rothfuss',  img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true }
             ]
         }
     },
@@ -14,7 +14,9 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-
+        toggleIsFav(book) {
+            book.isFav = !book.isFav
+        }
     }
 })
 
